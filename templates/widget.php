5e45a739935ef322ec9207c8c34b432860703abe
Jonas Brink <jobrink@uos.de>
2021-10-22 18:07:07 +0200

    added dummy content to widget on Startseite

---
+++ templates/widget.php
@@ -0,0 +1,10 @@
+<div>
+    <div>
+        <p>
+            Hallo hier steht der Info-Text zu teachUOS.
+        </p>
+    </div>
+    <footer>
+        <img src="<?= $oska_footer_image_url ?>">
+    </footer>
+</div>
