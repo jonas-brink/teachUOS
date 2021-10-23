9ec8a2a5f8f88d9a6e678052199b63311088bb42
Jonas Brink <jobrink@uos.de>
2021-10-23 12:42:48 +0200

    switch from PluginEngine to URLHelper

--- templates/widget.php
+++ templates/widget.php
@@ -1,6 +1,6 @@
 <article class="studip">
     <section>
         <p><?= _('Info-Text zu teachUOS.') ?></p>
-        <?= \Studip\LinkButton::create(_('teachUOS für mich aktivieren'), PluginEngine::getURL('teachUOS/pages/cw', ['cid' => 22, 'selected' => 33]), []) ?>
+        <?= \Studip\LinkButton::create(_('teachUOS für mich aktivieren'), URLHelper::getLink('teachUOS/pages/cw', ['cid' => 22, 'selected' => 33]), []) ?>
     </section>
 </article>
