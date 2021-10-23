977ac88b7f19a77b237c8253f2724562fb9255d5
Jonas Brink <jobrink@uos.de>
2021-10-23 12:47:22 +0200

    added access to new course

--- templates/widget.php
+++ templates/widget.php
@@ -1,6 +1,6 @@
 <article class="studip">
     <section>
         <p><?= _('Info-Text zu teachUOS.') ?></p>
-        <?= \Studip\LinkButton::create(_('teachUOS für mich aktivieren'), URLHelper::getLink('teachUOS/pages/cw', ['cid' => 22, 'selected' => 33]), []) ?>
+        <?= \Studip\LinkButton::create(_('teachUOS für mich aktivieren'), URLHelper::getLink('dispatch.php/course/enrolment/apply/' . 'eada44db9b6e295ff854eefba68c7601'), []) ?>
     </section>
 </article>
