3c11c8f4ffd72c9c0f0e1b5a5b85617d63be7617
Jonas Brink <jobrink@uos.de>
2021-10-23 12:33:00 +0200

    fix link of teachUOS widget buttton

--- templates/widget.php
+++ templates/widget.php
@@ -1,6 +1,6 @@
 <article class="studip">
     <section>
         <p><?= _('Info-Text zu teachUOS.') ?></p>
-        <?= Studip\Button::create(_('teachUOS für mich aktivieren'), PluginEngine::getURL('teachUOS/pages/cw'), ['class' => 'oska-find']) ?>
+        <?= Studip\Button::create(_('teachUOS für mich aktivieren'), PluginEngine::getURL('teachUOS/pages/cw'), []) ?>
     </section>
 </article>
