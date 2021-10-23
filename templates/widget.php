afc1956acfc3e1d45dd093489fa23e62dc72128c
Jonas Brink <jobrink@uos.de>
2021-10-23 11:51:40 +0200

    added button to teachUOS widget

--- templates/widget.php
+++ templates/widget.php
@@ -1,5 +1,6 @@
 <article class="studip">
     <section>
-        <?= _('Info-Text zu teachUOS.') ?>
+        <p><?= _('Info-Text zu teachUOS.') ?></p>
+        <?= Studip\Button::create(_('teachUOS für mich aktivieren'), 'oska_matching_button', ['class' => 'oska-find']) ?>
     </section>
 </article>
