343ca07da2d1b14c8b86972d80a9ba13b1498fae
Jonas Brink <jobrink@uos.de>
2021-10-23 11:48:57 +0200

    test new styling of teachUOS widget

--- templates/widget.php
+++ templates/widget.php
@@ -1,6 +1,6 @@
 <article class="studip">
     <section>
         <?= _('Info-Text zu teachUOS.') ?>
-        <?= Studip\Button::create(_('teachUOS für mich aktivieren'), 'oska_matching_button', ['class' => 'oska-find']) ?>
+        <?= /** Studip\Button::create(_('teachUOS für mich aktivieren'), 'oska_matching_button', ['class' => 'oska-find']) */ ?>
     </section>
 </article>
