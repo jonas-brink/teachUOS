b2a02649d8f2958db93aecdb65546152520b000d
Jonas Brink <jobrink@uos.de>
2021-10-23 12:36:30 +0200

    added dummy link to teachUOS widget button

--- templates/widget.php
+++ templates/widget.php
@@ -1,6 +1,6 @@
 <article class="studip">
     <section>
         <p><?= _('Info-Text zu teachUOS.') ?></p>
-        <?= \Studip\LinkButton::create(_('teachUOS für mich aktivieren'), 'http://example.org', []) ?>
+        <?= \Studip\LinkButton::create(_('teachUOS für mich aktivieren'), PluginEngine::getURL('teachUOS/pages/cw', ['cid' => 22, 'selected' => 33]), []) ?>
     </section>
 </article>
